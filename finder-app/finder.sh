87ac240d4338075d854684189e7c1ea8c5313f8e
Vignesh-Vadivel <viva9969@colorado.edu>
2023-01-23 18:10:08 -0700

    Added Comments and passes all Test cases

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -3,19 +3,25 @@
 filesdir=$1
 searchstr=$2
 
+#Checking for number of arguments.
 if [ $# -ne 2 ]; then
     echo "Invalid Number of arguments"
     exit 1
 fi
 
+#Check if it is a valid directory or not.
 if [ ! -d "$filesdir" ]; then
     echo "FILESDIR DOES NOT REPRESENT A DIRECTORY IN FILE SYSTEM"
     exit 1
 fi
 
+#Calculate total files present in the file system for present directory
 totalfiles=$(find $filesdir -type f | wc -l)
+
+#To store matching lines
 matchinglines=0
 
+#To check how many matching lines are present.
 for i in $(find $filesdir -type f)
 do
     if grep -q "$searchstr" "$i"; then
