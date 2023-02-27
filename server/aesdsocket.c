d8d85ed1db3c94a735814ee4481d441a0ce68665
Vignesh-Vadivel <viva9969@colorado.edu>
2023-02-26 19:37:53 -0700

    IP_V4 update

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -244,7 +244,7 @@ int main(int argc, char* argv[])
   /****************************** GET ADDRESS INFO ******************************/
   struct addrinfo hints, *res;
   memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
+  hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
